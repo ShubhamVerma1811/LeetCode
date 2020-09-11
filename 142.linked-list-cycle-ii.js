dbe6487b46ad5d967363926dc3b7fa52e6556d34
Shubham Verma <imshubhamverma.sv@gmail.com>
2020-09-11 07:25:42 +0530

    solved: #142

---
+++ 142.linked-list-cycle-ii.js
@@ -0,0 +1,101 @@
+/*
+ * @lc app=leetcode id=142 lang=javascript
+ *
+ * [142] Linked List Cycle II
+ *
+ * https://leetcode.com/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (36.11%)
+ * Likes:    3035
+ * Dislikes: 242
+ * Total Accepted:    348.1K
+ * Total Submissions: 924.3K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * Given a linked list, return the node where the cycle begins. If there is no
+ * cycle, return null.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which
+ * represents the position (0-indexed) in the linked list where tail connects
+ * to. If pos is -1, then there is no cycle in the linked list.
+ *
+ * Note: Do not modify the linked list.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [3,2,0,-4], pos = 1
+ * Output: tail connects to node index 1
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * second node.
+ *
+ *
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1,2], pos = 0
+ * Output: tail connects to node index 0
+ * Explanation: There is a cycle in the linked list, where tail connects to the
+ * first node.
+ *
+ *
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1], pos = -1
+ * Output: no cycle
+ * Explanation: There is no cycle in the linked list.
+ *
+ *
+ *
+ *
+ *
+ *
+ * Follow-up:
+ * Can you solve it without using extra space?
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+
+    if (fast === slow) {
+      return getLoopHead(slow, head);
+    }
+  }
+  return null;
+};
+
+const getLoopHead = (slow, head) => {
+  let fast = head;
+  while (slow !== fast) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return slow;
+};
+// @lc code=end
